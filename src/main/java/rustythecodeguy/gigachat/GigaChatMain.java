ee4eb66b822c6499c1e7f0d08e0927f7c466b786
NullPointerException <61092992+cd-con@users.noreply.github.com>
2022-01-04 14:15:34 +0300

    0.1

--- src/main/java/rustythecodeguy/gigachat/GigaChatMain.java
+++ src/main/java/rustythecodeguy/gigachat/GigaChatMain.java
@@ -69,15 +69,16 @@ public class GigaChatMain {
                 playerMessageChannel.addMember(player);
 
                 playerMessageChannel.send(Text.of(""));
-                playerMessageChannel.send(Text.builder("Sieg heil, " + player.getName() + "!").color(TextColors.GOLD).build());
-                playerMessageChannel.send(Text.builder("Чат без '!' действует в радиусе 32 блоков").build());
-                playerMessageChannel.send(Text.builder("Для того, чтобы высказаться в глобальный чат, напиши в начале сообщения '!'").build());
+                playerMessageChannel.send(Text.builder("Hello, " + player.getName() + "!").color(TextColors.GOLD).build());
+                playerMessageChannel.send(Text.builder("Sending messages without '!' avaliable in radius of 32 blocks").build());
+                playerMessageChannel.send(Text.builder("To use the global chat, write '!' at the beginning of the message").build());
 
             }
         }
 
         @Listener
         public void onPlayerLeave(ClientConnectionEvent.Disconnect event) {
+            // Need to remove?    
             Player player = event.getTargetEntity();
             logger.info("Player %player_name% left from server, so removing him from global chat".replace("%player_name%", player.getName()));
             globalChannel.removeMember(player);
@@ -87,23 +88,30 @@ public class GigaChatMain {
         public void onChat(MessageChannelEvent.Chat event) {
             Optional<Player> optionalPlayer = event.getCause().first(Player.class);
             if(optionalPlayer.isPresent()) {
+                // Get player, if avaliable
                 final Player player = optionalPlayer.get();
-
+                
+                // Create local channel and add event caller into it
                 AbstractMutableMessageChannel localChannel = new AbstractMutableMessageChannel(){};
                 localChannel.clearMembers();
                 localChannel.addMember(player);
-
+                
+                // Get message text
                 Text message = event.getMessage();
                 String messageString = message.toPlain();
                 String clearText = messageString.replace(player.getName(), "").replace("<","").replace(">", "");
-
+                
+                // Detect '!'
                 if (clearText.replace(" ", "").charAt(0) == "!".charAt(0))
-                {
+                {       
+                    // Global chat code
                     globalChannel.send(applyMinecraftFormat(Text.of(TextColors.GREEN, "[G] ", getPrefix(player), TextColors.RESET, player.getName(), getSuffix(player), TextColors.RESET, ":", clearText.replace("!", ""))));
                 }
                 else {
-                    final double localChatRadius = 18;
-
+                    // Local chat code
+                    final double localChatRadius = 32;
+                    
+                    //Get nearby players and add them in local channel
                     Collection<Entity> entities = player.getNearbyEntities(localChatRadius);
                     Iterator<Entity> iterator = entities.iterator();
                     Entity entity;
@@ -115,9 +123,13 @@ public class GigaChatMain {
                             localChannel.addMember(targetPlayer);
                         }
                     }
+                        
+                    // Send message
                     localChannel.send(applyMinecraftFormat(Text.of(TextColors.YELLOW, "[L] ", getPrefix(player), TextColors.RESET, player.getName(), getSuffix(player), TextColors.RESET, ":", clearText)));
+                    
+                    // Show message for player, if he was alone
                     if ((long) localChannel.getMembers().size() < 2){
-                        player.sendMessage(Text.of(TextColors.RED, "Тебя никто не услышал"));
+                        player.sendMessage(Text.of(TextColors.RED, "Nobody heard you"));
                     }
                 }
                 event.setCancelled(true);
@@ -125,10 +137,11 @@ public class GigaChatMain {
         }
         @Listener
         public void onServerStopped(GameStoppedEvent event) {
-            logger.info("GigaChat has been stopped! See ya later!");
+            logger.info("GigaChat has been stopped! See you later!");
         }
 
         private String getPrefix(Player player){
+            // TODO implement more permission plugins
             if (!permissionProvider_LuckPerms.isPresent()){
                 logger.warn("Permission service (LuckPerms) not found! Prefixes was disabled!");
                 return "";
@@ -143,6 +156,7 @@ public class GigaChatMain {
         }
 
         private String getSuffix(Player player){
+            // TODO implement more permission plugins
             if (!permissionProvider_LuckPerms.isPresent()){
                 logger.warn("Permission service (LuckPerms) not found! Prefixes was disabled!");
                 return "";
